7557aad7d096aa474f9f15ba36c2a402c881e1dc
Erfan Saberi <erfansaberiow@gmail.com>
2023-01-30 13:44:53 +0330

    Added Homepage

--- Simple_LMS/views.py
+++ Simple_LMS/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+# Homepage
+class HomePageView(TemplateView):
+    template_name = 'index.html'
