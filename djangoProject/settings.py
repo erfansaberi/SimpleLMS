ade751f6c8eabc137c2cb9117c2fb2c2e409d667
Erfan Saberi <erfansaberiow@gmail.com>
2023-01-31 10:48:00 +0330

    media root

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -120,6 +120,8 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [BASE_DIR / 'static',]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
+MEDIA_URL = 'uploads/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
