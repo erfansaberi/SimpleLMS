7ccae820802e87af6df9465ab8349df0acfee52a
Mostafa <mostafakooti2018@gmail.com>
2023-01-31 09:50:09 +0330

    Homework and solution almost done

--- Simple_LMS/urls.py
+++ Simple_LMS/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.contrib.auth.decorators import login_required
+
 from . import views
 
 urlpatterns = [
@@ -8,6 +8,7 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.RegisterPageView.as_view(), name='register'),
     path('dashboard/', views.DashboardPageView.as_view(), name='dashboard'),
+    path('solution-upload/', views.SolutionUpload.as_view(), name='solution'),
     path('dashboard/notifications', views.NotificationsPageView.as_view(), name='notifications'),
     path('dashboard/videos', views.VideosPageView.as_view(), name='videos'),
 ]
--- Simple_LMS/views.py
+++ Simple_LMS/views.py
@@ -1,30 +1,35 @@
-from django.shortcuts import redirect, render
-from django.views.generic import TemplateView
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpRequest
+from django.shortcuts import redirect, render
+from django.views import View
+from django.views.generic import TemplateView
+
 from .models import *
+
+
 # Create your views here.
 
 # Homepage
 class HomePageView(TemplateView):
     template_name = 'index.html'
 
+
 # Login
 class LoginPageView(TemplateView):
     template_name = 'login.html'
-    
+
     def post(self, request):
         email = request.POST.get('email')
         password = request.POST.get('password')
-        
+
         user = User.objects.filter(email=email)
         if not user:
             messages.warning(request, 'Username and passowrd does not match')
             print('User does not exist')
             return render(request, self.template_name)
-        
+
         username = user[0].username
         if (authenticate(request, username=username, password=password)):
             login(request, authenticate(request, username=username, password=password))
@@ -33,6 +38,7 @@ class LoginPageView(TemplateView):
         else:
             messages.warning(request, 'Username and passowrd does not match')
             return render(request, self.template_name)
+
 
 # Logout
 def logout_view(request):
@@ -43,10 +49,11 @@ def logout_view(request):
     messages.success(request, 'Logout successful')
     return redirect('/')
 
+
 # Register
 class RegisterPageView(TemplateView):
     template_name = 'register.html'
-    
+
     def post(self, request):
         username = request.POST.get('studentid')
         email = request.POST.get('email')
@@ -54,49 +61,67 @@ class RegisterPageView(TemplateView):
         confirm_password = request.POST.get('confirmpassword')
         firstname = request.POST.get('firstname')
         lastname = request.POST.get('lastname')
-        
+
         if (password != confirm_password):
             messages.warning(request, 'Password and confirm password does not match')
             return render(request, self.template_name)
-        
+
         user = User.objects.filter(username=username)
         if user:
             messages.warning(request, 'Student Id already exists')
             return render(request, self.template_name)
-        
+
         user = User.objects.filter(email=email)
         if user:
             messages.warning(request, 'Email already exists')
             return render(request, self.template_name)
-        
-        user = User.objects.create_user(username=username, email=email, password=password, first_name=firstname, last_name=lastname)
+
+        user = User.objects.create_user(username=username, email=email, password=password, first_name=firstname,
+                                        last_name=lastname)
         user.save()
         messages.success(request, 'Registration successful')
         return redirect('login')
-    
+
+
 # Dashboard
-class DashboardPageView(LoginRequiredMixin ,TemplateView):
+class DashboardPageView(LoginRequiredMixin, TemplateView):
     login_url = 'login'
     template_name = 'dashboard.html'
-    
+
     def get(self, request):
         notifications = Notification.objects.order_by('-id')[0:5]
-        return render(request, self.template_name, {'notifications': notifications})
-    
+        active_homework = Homework.objects.filter(is_active=True).first()
+        context = {'notifications': notifications, 'active_homework': active_homework}
+        return render(request, self.template_name, context)
 
-class NotificationsPageView(LoginRequiredMixin ,TemplateView):
+
+class SolutionUpload(View):
+    def post(self, request: HttpRequest):
+        user = request.user
+        homework_id = request.POST.get('homework_id')
+        user_solution = request.POST.get('solution')
+        print(user)
+        print(homework_id)
+        print(user_solution)
+        homework = Homework.objects.filter(id=homework_id).first()
+        solution = Solution(student=user, home_work=homework, file=user_solution)
+        solution.save()
+        return redirect(self.request.META.get('HTTP_REFERER'))
+
+
+class NotificationsPageView(LoginRequiredMixin, TemplateView):
     login_url = 'login'
     template_name = 'notifications.html'
-    
+
     def get(self, request):
         notifications = Notification.objects.order_by('-id')
         return render(request, self.template_name, {'notifications': notifications})
-    
-    
-class VideosPageView(LoginRequiredMixin ,TemplateView):
+
+
+class VideosPageView(LoginRequiredMixin, TemplateView):
     login_url = 'login'
     template_name = 'videos.html'
-    
+
     def get(self, request):
         videos = Video.objects.order_by('-id')
         return render(request, self.template_name, {'videos': videos})
